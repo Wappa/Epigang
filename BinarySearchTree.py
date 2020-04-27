85737d5f3ee2b54209e124af97adc264f9e70191
Wappa <aurelien.isak@epita.fr>
2020-04-28 00:14:41 +0200

    Kiev

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -4,6 +4,29 @@ I Hope you will found it useful.
 
 Have Fun my friend !
 
-S/o Apero 
+S/o Apero
 S/o Belveteh
 """
+def search(x):
+    # Base Cases: root is null or key is present at root
+    if B.key == None or B == None:
+        return B.key
+    if x < B.key :
+        return search(x.left.key)
+    if x > B.key:
+        return search(x.right.key)
+
+def insert(x):
+    if B.key == None:
+        return x
+    else:
+        if x > B.key:
+            if B.right == None:
+                B.right = x
+            else:
+                insert(B.right.key)
+        else:
+            if B.left == None:
+                B.left = x
+            else:
+                insert(B.left.key)
