6fa82208c8fee0691c3665cb617f7446b254a447
Wappa <aurelien.isak@epita.fr>
2020-05-28 21:50:56 +0200

    Work String C# Finish

--- C#/string.cs
+++ C#/string.cs
@@ -48,3 +48,55 @@ namespace NameGrab
   }
 }
 
+namespace MovieScript
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Script line
+      string script = "Close on a portrait of the HANDSOME PRINCE -- as the BEAST'S giant paw slashes it.";
+
+      // Get camera directions
+      int charPosition = script.IndexOf("Close");
+      int length = "Close on".Length;
+      string cameraDirections = script.Substring(charPosition, length);
+
+      // Get scene description
+      charPosition = script.IndexOf("a portrait");
+      string sceneDescription = script.Substring(charPosition);
+
+      // Make camera directions uppercase
+      cameraDirections = cameraDirections.ToUpper();
+
+      // Make scene description lowercase
+      sceneDescription = sceneDescription.ToLower();
+
+      // Print results
+      Console.WriteLine($"{cameraDirections} {sceneDescription}");
+    }
+  }
+}
+
+
+namespace Review
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      /* use this space to write your own short program!
+      Here's what you learned:
+
+      DATA TYPES: char, string
+      STRING INTERPOLATION: $"blah blah"
+      STRING INFO: .Length, .IndexOf()
+      PARTS OF STRINGS: bracketNotation[], .Substring()
+      STRING MANIPULATION: .ToUpper(), .ToLower()
+
+      Good luck! */
+
+
+    }
+  }
+}
