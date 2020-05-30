deff4bbd32d8c6b5a48e621013e975b9fccce7e6
Wappa <aurelien.isak@epita.fr>
2020-05-30 12:53:31 +0200

    Start Input Csh

---
+++ C#/CallsAndInput.cs
@@ -0,0 +1,15 @@
+namespace CallAMethod
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string msg = "Yabba dabba doo!";
+      Math.Min(5,2);
+      Console.WriteLine(msg);
+      msg.Substring(0,1);
+    }
+  }
+}
+
+
