443d9cf673ece9a93435a530646468cd5383dfa8
Wappa <aurelien.isak@epita.fr>
2020-05-31 17:06:32 +0200

    Alternate expression Lambda Work Finish C#

--- C#/AlternateExpressions.cs
+++ C#/AlternateExpressions.cs
@@ -87,3 +87,48 @@ namespace AlternateExpressions
     }
   }
 }
+
+namespace AlternateExpressions
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] spaceRocks = {"meteoroid", "meteor", "meteorite"};
+
+      bool makesContact = Array.Exists(spaceRocks, s => s == "meteorite");
+
+      if (makesContact)
+      {
+        Console.WriteLine("At least one space rock has reached the Earth's surface!");
+      }
+    }
+  }
+}
+
+namespace AlternateExpressions
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int[] nums = {0, 555, 252, 3, 9, 101};
+
+      bool hasBigNum = Array.Exists(nums, isBig);
+
+      bool hasSmallNum = Array.Exists(nums, isSmall);
+
+      bool hasMediumNum = Array.Exists(nums, (n) => n >= 10 && n <= 100);
+
+      Console.WriteLine($"Any big #s? {hasBigNum}");
+      Console.WriteLine($"Any small #s? {hasSmallNum}");
+      Console.WriteLine($"Any medium #s? {hasMediumNum}");
+
+    }
+
+    static bool isBig(int n) => n > 100;
+
+    static bool isSmall(int n) => n < 10;
+
+  }
+}
