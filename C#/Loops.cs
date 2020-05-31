76c1fea7d378860b3bf4a60660cf2e17922657bd
Wappa <aurelien.isak@epita.fr>
2020-05-31 20:00:45 +0200

    Loop Finish Csharp

--- C#/Loops.cs
+++ C#/Loops.cs
@@ -53,3 +53,81 @@ namespace ForLoop
     }
   }
 }
+
+namespace ForEachLoop
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] todo = { "respond to email", "make wireframe", "program feature", "fix bugs" };
+
+      foreach(string task in todo)
+      {
+          CreateTodoItem(task);
+      }
+    }
+
+    static void CreateTodoItem(string item)
+    {
+      Console.WriteLine($"[] {item}");
+    }
+  }
+}
+
+namespace ComparingLoops
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] websites = { "twitter", "facebook", "gmail" };
+
+      foreach(string site in websites)
+      {
+        Console.WriteLine(site);
+      }
+    }
+  }
+}
+
+namespace JumpStatements
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      bool buttonClick = true;
+      int ring = 0;
+
+      do
+      {
+        Console.WriteLine("BLARRRRR");
+        ring += 1;
+        if(ring ==3){
+          break;
+        }
+      } while(!buttonClick && ring <3);
+    }
+  }
+}
+
+namespace LoopsReview
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      /* use this space to write your own short program!
+      Here's what you learned:
+
+      while loop: while(){..}
+      do...while loop: do{...}while();
+      for loop: for(int i=0; i<x; i++){}
+      foreach loop: foreach(int item in list){}
+      jump statements: break, continue, return
+
+      Good luck! */
+    }
+  }
+}
