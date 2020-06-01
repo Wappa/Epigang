67e7af1266cbe336efe0c79a7e6dee22c8c2dc7e
Wappa <aurelien.isak@epita.fr>
2020-06-01 19:18:14 +0200

    Calss finish C#

--- C#/Classes.cs
+++ C#/Classes.cs
@@ -267,3 +267,244 @@ namespace BasicClasses
     }
   }
 }
+
+namespace BasicClasses
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Forest f = new Forest("Congo", "Tropical");
+      f.Trees = 0;
+
+      Console.WriteLine(f.Name);
+      Console.WriteLine(f.Biome);
+    }
+  }
+}
+
+namespace BasicClasses
+{
+  class Forest
+  {
+    public int age;
+    private string biome;
+
+    public Forest(string name, string biome)
+    {
+      Name = name;
+      Biome = biome;
+      Age = 0;
+    }
+
+    public string Name
+    { get; set; }
+
+    public int Trees
+    { get; set; }
+
+    public string Biome
+    {
+      get { return biome; }
+      set
+      {
+        if (value == "Tropical" ||
+            value == "Temperate" ||
+            value == "Boreal")
+        {
+          biome = value;
+        }
+        else
+        {
+          biome = "Unknown";
+        }
+      }
+    }
+
+    public int Age
+    {
+      get { return age; }
+      private set { age = value; }
+    }
+
+    public int Grow()
+    {
+      Trees += 30;
+      Age += 1;
+      return Trees;
+    }
+
+    public int Burn()
+    {
+      Trees -= 20;
+      Age += 1;
+      return Trees;
+    }
+  }
+}
+
+namespace BasicClasses
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Forest f = new Forest("Congo", "Tropical");
+      f.Trees = 0;
+
+      Console.WriteLine(f.Name);
+      Console.WriteLine(f.Biome);
+
+      Forest r = new Forest("Rendlesham");
+      Console.WriteLine(r.Biome);
+    }
+  }
+}
+
+namespace BasicClasses
+{
+  class Forest
+  {
+    public int age;
+    private string biome;
+
+    public Forest(string name, string biome)
+    {
+      this.Name = name;
+      this.Biome = biome;
+      Age = 0;
+    }
+
+    public Forest(string name) : this(name, "Unknown")
+    {
+      Console.WriteLine("Biome not specified. Value defaulted to 'Unknown'.");
+    }
+
+    public string Name
+    { get; set; }
+
+    public int Trees
+    { get; set; }
+
+    public string Biome
+    {
+      get { return biome; }
+      set
+      {
+        if (value == "Tropical" ||
+            value == "Temperate" ||
+            value == "Boreal")
+        {
+          biome = value;
+        }
+        else
+        {
+          biome = "Unknown";
+        }
+      }
+    }
+
+    public int Age
+    {
+      get { return age; }
+      private set { age = value; }
+    }
+
+    public int Grow()
+    {
+      Trees += 30;
+      Age += 1;
+      return Trees;
+    }
+
+    public int Burn()
+    {
+      Trees -= 20;
+      Age += 1;
+      return Trees;
+    }
+
+  }
+
+}
+
+namespace BasicClasses
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Forest a = new Forest("Amazon");
+      Console.WriteLine(a.Trees);
+      a.Grow();
+      Console.WriteLine(a.Trees);
+
+    }
+  }
+}
+
+namespace BasicClasses
+{
+  class Forest
+  {
+    public int age;
+    private string biome;
+
+    public Forest(string name, string biome)
+    {
+      this.Name = name;
+      this.Biome = biome;
+      Age = 0;
+    }
+
+    public Forest(string name) : this(name, "Unknown")
+    { }
+
+    public string Name
+    { get; set; }
+
+    public int Trees
+    { get; set; }
+
+    public string Biome
+    {
+      get { return biome; }
+      set
+      {
+        if (value == "Tropical" ||
+            value == "Temperate" ||
+            value == "Boreal")
+        {
+          biome = value;
+        }
+        else
+        {
+          biome = "Unknown";
+        }
+      }
+    }
+
+    public int Age
+    {
+      get { return age; }
+      private set { age = value; }
+    }
+
+    public int Grow()
+    {
+      Trees += 30;
+      Age += 1;
+      return Trees;
+    }
+
+    public int Burn()
+    {
+      Trees -= 20;
+      Age += 1;
+      return Trees;
+    }
+
+  }
+
+}
+
