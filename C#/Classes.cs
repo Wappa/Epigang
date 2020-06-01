95ab377fd623f3fb61028d553e06f65aa10bb255
Wappa <aurelien.isak@epita.fr>
2020-06-01 17:20:16 +0200

    Class 7/12 C#

--- C#/Classes.cs
+++ C#/Classes.cs
@@ -160,3 +160,58 @@ namespace BasicClasses
   }
 }
 
+namespace BasicClasses
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Forest f = new Forest();
+      f.Name = "Congo";
+      f.Trees = 0;
+      f.Age = 0;
+      f.Biome = "Desert";
+
+      Console.WriteLine(f.Name);
+      Console.WriteLine(f.Biome);
+    }
+  }
+}
+
+namespace BasicClasses
+{
+  class Forest
+  {
+    public int age;
+    public int Age
+    {
+      get{ return age; }
+      private set {age = value;}
+    }
+    private string biome;
+
+    public string Name
+    { get; set; }
+
+    public int Trees
+    { get; set; }
+
+    public string Biome
+    {
+      get { return biome; }
+      set
+      {
+        if (value == "Tropical" ||
+            value == "Temperate" ||
+            value == "Boreal")
+        {
+          biome = value;
+        }
+        else
+        {
+          biome = "Unknown";
+        }
+      }
+    }
+  }
+}
