df8d8f1b5b655eaef79c18dc216b0d28197553a9
Wappa <aurelien.isak@epita.fr>
2020-06-27 13:06:18 +0200

    First cpp commit

--- C#/CodeCademy/Interface.cs
+++ C#/CodeCademy/Interface.cs
@@ -10,3 +10,35 @@ namespace LearnInterfaces
       void Honk();
     }
 }
+
+namespace LearnInterfaces
+{
+  class Sedan : IAutomobile
+  {
+    public string LicensePlate
+    { get; }
+
+    public double Speed
+    { get; }
+
+    public int Wheels
+    { get; }
+
+    public void Honk()
+    {
+      Console.WriteLine("HONK!");
+    }
+
+  }
+}
+
+namespace LearnInterfaces
+{
+  interface IAutomobile
+  {
+    string LicensePlate { get; }
+    double Speed { get; }
+    int Wheels { get; }
+    void Honk();
+  }
+}
