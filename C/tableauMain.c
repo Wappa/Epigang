d837657a552eb33ae8b30f50905b56b2278c77ab
Wappa <aurelien.isak@epita.fr>
2020-07-16 19:23:14 +0200

    Yolo

--- C/tableauMain.c
+++ C/tableauMain.c
@@ -5,7 +5,10 @@
 int main()
 {
     int tableau[4] = {1,2,3,4};
+    int teblo [5] = {5,1,4,2,8};
+    int Copie[4] = {0,0,0,0};
     //printf("%d \n",sommeTableau(tableau, 4));
-    printf("%f \n", moyenneTableau(tableau,4));
+    ordonnerTableau(teblo,5);
+    printf("\n");
     return 0;
 }
--- C/travailTableau.h
+++ C/travailTableau.h
@@ -7,6 +7,7 @@ double moyenneTableau(int tableau[], int tailleTableau);
 void copie(int tableauOriginal[], int tableauCopie[], int tailleTableau);
 void maximumTableau(int tableau[], int tailleTableau, int valeurMax);
 void ordonnerTableau(int tableau[], int tailleTableau);
+//Fonction Annexe:
 
 int sommeTableau(int tableau[], int tailleTableau)
 {
@@ -21,7 +22,67 @@ int sommeTableau(int tableau[], int tailleTableau)
 
 double moyenneTableau(int tableau[], int tailleTableau)
 {
-    //A refaire avec pointeurs
-    int moyenne = sommeTableau(tableau,tailleTableau)/tailleTableau;
+    //Cast des valeurs en float
+    float sumtab = (float) sommeTableau(tableau,tailleTableau);
+    float taille = (float) tailleTableau;
+    float moyenne = sumtab/taille;
     return moyenne;
+}
+
+void copie (int tableauOriginal[], int tableauCopie[], int tailleTableau)
+{
+    for (int i = 0; i < tailleTableau; i++)
+    {
+        tableauCopie[i] = tableauOriginal[i];
+    }
+    for (int j = 0; j < tailleTableau ; j++)
+    {
+        printf("%d", tableauOriginal[j]);
+    }
+    printf("\n");
+    for(int k = 0 ; k  <tailleTableau ; k++)
+    {
+        printf("%d", tableauCopie[k]);
+    }
+    printf("\n");
+}
+
+void MaximumTableau (int tableau[],int tailleTableau , int valeurMax)
+{
+    for (int i = 0 ; i < tailleTableau ; i++)
+    {
+        if(tableau[i] > valeurMax)
+        {
+            tableau[i] = 0;
+        }
+    }
+    for(int j = 0 ; j < tailleTableau ; j++)
+    {
+        printf("%d", tableau[j]);
+    }
+    printf("\n");
+}
+
+
+void swap(int *xp, int *yp) 
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+void ordonnerTableau(int arr[], int n)
+{
+   int i, j;
+   for (i = 0; i < n-1; i++)
+
+       // Last i elements are already in place
+       for (j = 0; j < n-i-1; j++)
+           if (arr[j] > arr[j+1])
+              swap(&arr[j], &arr[j+1]);
+
+   for(int k = 0 ; k < n ; k++)
+   {
+       printf("%d", arr[k]);
+   }
 }
